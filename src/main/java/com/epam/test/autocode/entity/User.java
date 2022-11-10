0215abd230242fa16248787becc2e9ae82f226e8
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2022-11-10 13:48:12 +0300

    first commit

---
+++ src/main/java/com/epam/test/autocode/entity/User.java
@@ -0,0 +1,22 @@
+package com.epam.test.autocode.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public User(Long id, Character startWith) {
+        this.id = id;
+        firstName = generateString(startWith.toString().toUpperCase() + "_first name ");
+        lastName = generateString(startWith.toString().toUpperCase() + "_last name ");
+        age = Math.toIntExact(this.id);
+    }
+
+    private String generateString(String str) {
+        return str;
+    }
+}
